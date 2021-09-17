959ab92ba58b89220b826c889800936d10bc007e
owner <yokoyamacode@gmail.com>
2021-09-17 15:04:58 +0900

    formated for pep8

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
-
+# noinspection PyStatementEffect
 setup(name='clappy',
       version='1.0',
       py_modules=['clappy'],
       author='Toshiyuki Yokoyama',
       author_email='yokoyamacode@gmail.com',
-      url = 'https://github.com/yoko72/clappy',
+      url='https://github.com/yoko72/clappy',
 
       )
 
