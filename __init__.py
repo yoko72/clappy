239a6ea1d3e1c82cd235ad8a17c7cab317cbffc3
owner <yokoyamacode@gmail.com>
2021-09-17 14:47:52 +0900

    added metadata

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
-from clappy import Parser
+from clappy import *
+
+__copyright__ = 'Copyright (C) 2021 Toshiyuki Yokoyama'
+__version__ = '1.0.0'
+__license__ = 'MIT'
+__author__ = 'Toshiyuki Yokoyama'
+__author_email__ = 'yokoyamacode@gmail.com'
+__url__ = 'https://github.com/yoko72/clappy'
