70b481d4fc63fad5d160b8335e129bcaf99efebd
owner <yokoyamacode@gmail.com>
2021-09-17 14:48:26 +0900

    1.0

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(name='clappy',
+      version='1.0',
+      py_modules=['clappy'],
+      author='Toshiyuki Yokoyama',
+      author_email='yokoyamacode@gmail.com',
+      url = 'https://github.com/yoko72/clappy',
+
+      )
+
+
