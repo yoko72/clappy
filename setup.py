5c32d282abcf9d8c370123d6161e08157e6d2291
owner <yokoyamacode@gmail.com>
2021-09-18 21:45:17 +0900

    fixed from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
-from distutils.core import setup
+from setuptools import setup
+
+# requires = ["requests>=3.2"]
 
 
 setup(name='clappy',
-      version='1.0',
+      version='0.1.0',
       py_modules=['clappy'],
+      description="Command line argument parser for pythonic code",
+      long_description="""Command Line Argument Parser for PYthonic code.
+
+    given_kwarg1 = clappy.parse("--kwarg1")
+    given_kwarg2 = clappy.parse("--kwarg2")
+    
+You can directly get each given arg.""",
+      classifiers=["Topic :: Software Development :: Libraries :: Python Modules",
+                   "Development Status :: 4 - Beta"],
       author='Toshiyuki Yokoyama',
       author_email='yokoyamacode@gmail.com',
       url='https://github.com/yoko72/clappy',
-      setup_requires=['wheel']
+      setup_requires=['wheel'],
+      # install_requires=requires,
+      keywords="command line argument parser"
       )
