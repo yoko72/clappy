1c21c8fdd98f7d8f8232dbcdbf5a9aedd551a445
owner <yokoyamacode@gmail.com>
2021-09-21 01:17:12 +0900

    updated __version__

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
-from clappy import *
-
 __copyright__ = 'Copyright (C) 2021 Toshiyuki Yokoyama'
-__version__ = '1.0.0'
+__version__ = '0.1.7'
 __license__ = 'MIT'
 __author__ = 'Toshiyuki Yokoyama'
-__author_email__ = 'yokoyamacode@gmail.com'
+__author_email__ = 'yokoyama19860721@gmail.com'
 __url__ = 'https://github.com/yoko72/clappy'
+
+from clappy import *
