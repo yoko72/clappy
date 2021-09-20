da1d32f1953b86d93a3c153c66892ca8de496f29
owner <yokoyamacode@gmail.com>
2021-09-21 01:18:14 +0900

    moved to setup.cfg

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
 from setuptools import setup
 
-
-setup(name='clappy',
-      version='0.1.2',
-      py_modules=['clappy'],
-      description="Command line argument parser for pythonic code",
-      long_description="""Command Line Argument Parser for PYthonic code.
-
-    given_kwarg1 = clappy.parse("--kwarg1")
-    given_kwarg2 = clappy.parse("--kwarg2")
-    
-You can directly get each given arg.""",
-      classifiers=["Topic :: Software Development :: Libraries :: Python Modules",
-                   "Development Status :: 4 - Beta"],
-      author='Toshiyuki Yokoyama',
-      author_email='yokoyamacode@gmail.com',
-      url='https://github.com/yoko72/clappy',
-      setup_requires=['wheel'],
-      python_requires='>=3.2',
-      # install_requires=requires,
-      keywords="command line argument parser"
-      )
+setup()
