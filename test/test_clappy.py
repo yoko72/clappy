aba310063f2a9b25ab5d5d4328f420b91914fa97
owner <yokoyamacode@gmail.com>
2021-09-27 03:46:51 +0900

    updated and added some tests

--- test/test_clappy.py
+++ test/test_clappy.py
@@ -1,47 +1,76 @@
 import clappy
 import unittest
 import argparse
+import sys
+import functools
+import inspect
 
 
-def get_parsed_args(args):
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-a", default=2)
-    parser.add_argument("--b1", default=7)
-    parser.add_argument("--flag", action="store_true")
-    parser.add_argument("positionals", nargs="*")
-    parsed_args = parser.parse_args(args)
-    return parsed_args
+bounds: list[inspect.BoundArguments] = []
 
 
-def get_parsed_args_with_sub(args):
-    parser = argparse.ArgumentParser()
-    # parser.add_argument("--kw", default=3)
-    subparsers = parser.add_subparsers()
-    subparser1 = subparsers.add_parser("sub")
-    subparser1.add_argument("--subopt")
-    # subparser2 = subparsers.add_parser("sub2")
-    # subparser2.add_argument("--second_sub_opt")
-    result = parser.parse_args(args)
-    return result
+def temp_argv(*outer_args):
+    def get_decorated_func(test_func):
+        @functools.wraps(test_func)
+        def wrapper(*args, **kwargs):
+            global bounds
+            sys.argv[1::] = outer_args
+            result = test_func(*args, **kwargs)
+            del sys.argv[1::]
+            clappy._parser = None
+            clappy._subparsers = None
+            bounds = []
+            return result
+        return wrapper
+    return get_decorated_func
+
+
+signature = inspect.signature(argparse.ArgumentParser().add_argument)
+
+
+def append_bound(*args, **kwargs):
+    argument_bound = signature.bind(*args, **kwargs)
+    bounds.append(argument_bound)
 
 
 class TestClappy(unittest.TestCase):
-    """Checks the result of parsing if it's same between clappy and argparse. """
-    def test_parse(self):
-        args = ("-a", "3", "--b1", "4", "5", "6")
-        namespace = get_parsed_args(args)
-        clappy.set_args_on_parse(*args)
-        self.assertEqual(namespace.a, clappy.parse("-a"))
-        self.assertEqual(namespace.b1, clappy.parse("--b1"))
-        self.assertEqual(namespace.flag, clappy.parse("--flag", action="store_true"))
-        self.assertEqual(namespace.positionals, clappy.parse("positionals", nargs="*"))
+    """Checks the result of parsing."""
 
+    @temp_argv("a", "b", "c")
+    def test_positional_arg(self):
+        self.assertEqual(clappy.parse("positionals", nargs="*"), ["a", "b", "c"])
+
+    @temp_argv("--kw", "hoge")
+    def test_kwarg(self):
+        self.assertEqual(clappy.parse("--kw"), "hoge")
+
+    @temp_argv("sub1", "--subkw", "foo")
     def test_subcommand(self):
-        args = ("sub", "--subopt", "3")
-        namespace = get_parsed_args_with_sub(args)
-        clappy.set_args_on_parse(*args)
-        subparser = clappy.get_subcommand_parser("sub")
-        self.assertEqual(namespace.subopt, subparser.parse("--subopt"))
+        subparser = clappy.get_subcommand_parser("sub1")
+        self.assertEqual(subparser.parse("--subkw"), "foo")
+
+    @temp_argv("-a", "3", "--b1", "4", "5", "6")
+    def test_parse(self):
+        append_bound("-a", default=2)
+        append_bound("--b1", default=7)
+        append_bound("--flag", action="store_true")
+        append_bound("positionals", nargs="*")
+
+        parser = argparse.ArgumentParser()
+        for bound in bounds:
+            parser.add_argument(*bound.args, **bound.kwargs)
+        namespace = parser.parse_args()
+
+        for bound in bounds:
+            try:
+                arg = bound.args[0]
+            except IndexError:
+                arg = bound.kwargs["dest"]
+            finally:
+                # noinspection PyUnboundLocalVariable
+                arg = arg.lstrip(parser.prefix_chars)
+            with self.subTest(arg=arg):
+                self.assertEqual(clappy.parse(*bound.args, **bound.kwargs), getattr(namespace, arg))
 
 
 if __name__ == '__main__':
