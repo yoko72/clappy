84e680b947d564f36af00ff6f94366d6bf3f81b4
owner <yokoyamacode@gmail.com>
2021-10-03 00:47:55 +0900

    moved file info to setup.cfg

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
-import pathlib
 from setuptools import setup
 
-
-def _read_readme():
-    return open((pathlib.Path() / 'README.md'), encoding='utf-8').read().replace("\r", "")
-
-
-setup(
-    long_description=_read_readme(),
-    long_description_content_type="text/markdown",
-)
+setup()
