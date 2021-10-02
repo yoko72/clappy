b6f247de3c236d30f174638fa41725ce00af4b9a
owner <yokoyamacode@gmail.com>
2021-10-02 10:26:35 +0900

    rst to md

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
+import pathlib
 from setuptools import setup
 
-setup()
+
+def _read_readme():
+    return open((pathlib.Path() / 'README.md'), encoding='utf-8').read().replace("\r", "")
+
+
+setup(
+    long_description=_read_readme(),
+    long_description_content_type="text/markdown",
+)
